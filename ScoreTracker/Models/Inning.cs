21e27f6521fd0745d6cf2412baf9c5ea2227a357
Vinay Menda <vmenda@iotap.com>
2021-03-30 02:11:02 +0530

    Add project files.

---
+++ ScoreTracker/Models/Inning.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreTracker.Models
+{
+    public class Inning
+    {
+        public Inning() { Runs = 0; Wickets = 0; Overs = 0.0; }
+
+        public double Overs { get; set; }
+
+        public int TotalOvers { get; set; }
+
+        public int Players { get; set; }
+
+        public int Runs { get; set; }
+
+        public int Wickets { get; set; }
+    }
+}
